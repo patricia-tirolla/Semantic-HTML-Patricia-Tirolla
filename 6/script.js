6dd957675cd57aa8609e652175d765edf15ace65
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2025-01-09 15:37:38 +0100

    Sets and gets local and session storages

--- 6/script.js
+++ 6/script.js
@@ -1,23 +1,26 @@
 // TODO: Set item in localStorage
-
+localStorage.setItem("name:", "Kevin");
 // TODO: Get item from localStorage
-
+localStorage.getItem("name:");
 // TODO: Remove item from localStorage
-
+localStorage.removeItem("name:");
 // TODO: Clear all items from localStorage
-
+localStorage.clear();
 // TODO: Add an object to localStorage
-
+const user1 = { name: 'Gisele', age: '43'};
+localStorage.setItem('user', JSON.stringify(user1));
 // TODO: Get an object from localStorage
-
+JSON.parse(localStorage.getItem('user'));
 // TODO: Set item in sessionStorage
-
+sessionStorage.setItem("name:", "kevin");
 // TODO: Get item from sessionStorage
-
+sessionStorage.getItem("name:");
 // TODO: Remove item from sessionStorage
-
+sessionStorage.removeItem("name:");
 // TODO: Clear all items from sessionStorage
-
+sessionStorage.clear();
 // TODO: Add an object to sessionStorage
-
+const user = { name: 'Gisele', age: '43'};
+sessionStorage.setItem('user', JSON.stringify(user));
 // TODO: Get an object from sessionStorage
+JSON.parse(sessionStorage.getItem('user'));
