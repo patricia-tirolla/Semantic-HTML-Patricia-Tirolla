7620384420b24ac1e4aab35dad1317d6dc9d7f65
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2025-01-17 14:42:58 +0100

    Add css to PersonProfile component

--- react-assignments/src/PersonProfile.jsx
+++ react-assignments/src/PersonProfile.jsx
@@ -1,22 +1,25 @@
 import React from "react"
+import "./PersonProfile.css"
 
 export default function PersonProfile(props) {
-    return (
+  return (
+    <div className='person-container'>
       <div>
-        <p>Hi!My name is <strong>{props.name}</strong>!</p>
-        <p>I am {props.info.age} years old.</p>
-        <p>Actually, I live in {props.info.city} and I work as a {props.info.job}.</p>
-        <p>What makes me happy to do on my free time is: </p>
-        <HobbiesList hobbies={props.info.hobbies} />
+        <h2>Hi! My name is <strong>{props.name}</strong>!</h2>
       </div>
-    )
-  }
-  function HobbiesList(props) {
-    return (
-      <ul>
-        {props.hobbies.map((hobby, index) => (
-          <li key={index}>- {hobby}</li>
-        ))}
-      </ul>
-    )
-  }
+      <p>I am {props.info.age} years old.</p>
+      <p>Actually, I live in {props.info.city} and I work as a {props.info.job}.</p>
+      <p>What makes me happy to do on my free time is: </p>
+      <HobbiesList hobbies={props.info.hobbies} />
+    </div>
+  )
+}
+function HobbiesList(props) {
+  return (
+    <ul>
+      {props.hobbies.map((hobby, index) => (
+        <li key={index}> {hobby}</li>
+      ))}
+    </ul>
+  )
+}
